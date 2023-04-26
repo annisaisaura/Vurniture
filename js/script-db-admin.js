f96218f5e521a9e872dcc0f6a97c813727fda19b
annisaisaura <annisa.isaura.alfiany-2021@fst.unair.ac.id>
2023-04-26 07:22:04 +0700

    update admin database

--- js/script-db-admin.js
+++ js/script-db-admin.js
@@ -17,6 +17,7 @@ function readFormData() {
     var formData = {};
     formData["productCode"] = document.getElementById("productCode").value;
     formData["product"] = document.getElementById("product").value;
+    // formData["img"] = document.getElementById("img").value;
     formData["qty"] = document.getElementById("qty").value;
     formData["perPrice"] = document.getElementById("perPrice").value;
     return formData;
@@ -27,15 +28,15 @@ function insertNewRecord(data) {
     var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-		cell1.innerHTML = data.productCode;
+		    cell1.innerHTML = data.productCode;
     cell2 = newRow.insertCell(1);
-		cell2.innerHTML = data.product;
+		    cell2.innerHTML = data.product;
     cell3 = newRow.insertCell(2);
-		cell3.innerHTML = data.qty;
+		    cell3.innerHTML = data.qty;
     cell4 = newRow.insertCell(3);
-		cell4.innerHTML = data.perPrice;
-    cell4 = newRow.insertCell(4);
-        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+		    cell4.innerHTML = data.perPrice;
+    cell5 = newRow.insertCell(4);
+        cell5.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 }
 
 //Edit the data
@@ -66,7 +67,87 @@ function onDelete(td) {
 function resetForm() {
     document.getElementById("productCode").value = '';
     document.getElementById("product").value = '';
+    // document.getElementById("img").value = '';
     document.getElementById("qty").value = '';
     document.getElementById("perPrice").value = '';
     selectedRow = null;
 }
+
+// var selectedRow = null
+
+// function onFormSubmit(e) {
+// 	event.preventDefault();
+//         var formData = readFormData();
+//         if (selectedRow == null){
+//             insertNewRecord(formData);
+// 		}
+//         else{
+//             updateRecord(formData);
+// 		}
+//         resetForm();    
+// }
+
+// //Retrieve the data
+// function readFormData() {
+//     var formData = {};
+//     formData["productCode"] = document.getElementById("productCode").value;
+//     formData["product"] = document.getElementById("product").value;
+//     formData["img"] = document.getElementById("img").value;
+//     formData["qty"] = document.getElementById("qty").value;
+//     formData["perPrice"] = document.getElementById("perPrice").value;
+//     return formData;
+// }
+
+// //Insert the data
+// function insertNewRecord(data) {
+//     var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+//     var newRow = table.insertRow(table.length);
+//     cell1 = newRow.insertCell(0);
+// 		    cell1.innerHTML = data.productCode;
+//     cell2 = newRow.insertCell(1);
+// 		    cell2.innerHTML = data.product;
+//     cell3 = newRow.insertCell(2);
+// 		    cell3.innerHTML = data.img;
+//     cell4 = newRow.insertCell(3);
+// 		    cell4.innerHTML = data.qty;
+//     cell5 = newRow.insertCell(4);
+// 		    cell5.innerHTML = data.perPrice;
+//     cell6 = newRow.insertCell(5);
+//        cell6.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+// }
+
+// //Edit the data
+// function onEdit(td) {
+//     selectedRow = td.parentElement.parentElement;
+//     document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+//     document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+//     document.getElementById("img").value = selectedRow.cells[2].innerHTML;
+//     document.getElementById("qty").value = selectedRow.cells[3].innerHTML;
+//     document.getElementById("perPrice").value = selectedRow.cells[4].innerHTML;
+// }
+// function updateRecord(formData) {
+//     selectedRow.cells[0].innerHTML = formData.productCode;
+//     selectedRow.cells[1].innerHTML = formData.product;
+//     selectedRow.cells[2].innerHTML = formData.img;
+//     selectedRow.cells[3].innerHTML = formData.qty;
+//     selectedRow.cells[4].innerHTML = formData.perPrice;
+// }
+
+// //Delete the data
+// function onDelete(td) {
+//     if (confirm('Do you want to delete this record?')) {
+//         row = td.parentElement.parentElement;
+//         document.getElementById('storeList').deleteRow(row.rowIndex);
+//         resetForm();
+//     }
+// }
+
+// //Reset the data
+// function resetForm() {
+//     document.getElementById("productCode").value = '';
+//     document.getElementById("product").value = '';
+//     document.getElementById("img").value = '';
+//     document.getElementById("qty").value = '';
+//     document.getElementById("perPrice").value = '';
+//     selectedRow = null;
+// }
